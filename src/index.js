0307382afc08fa09b45c196e614f6701ce47b8a5
claytonm17 <133607487+claytonm17@users.noreply.github.com>
2024-01-01 10:38:15 -0500

    Create console log to do list and DOM creation of html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
 import './styles.css';
 
-console.log('Hello World')
+class ToDoItem {
+    constructor(task, isComplete) {
+        this.task = task;
+        this.isComplete = isComplete;
+    }
+}
+
+const toDoList = [];
+
+const addNewItem = (title, isComplete) => {
+    const newItem = new ToDoItem(title, isComplete)
+    toDoList.push(newItem);
+}
+
+const removeItem = (index) => {
+    toDoList.splice(index, 1)
+}
+
+addNewItem('Do the dishes', true)
+addNewItem('Walk the dog', false)
+addNewItem('Laundry', false)
+
+const readToDoList = () => {
+    for (let i = 0; i < toDoList.length; i++) {
+        console.log(toDoList[i].task)
+        console.log(toDoList[i].isComplete)
+    }
+}
+
+readToDoList()
+
+// DOM creation
+// 
+const content = document.querySelector("#content");
+
+const buildTitle = () => {
+    const parent = content
+    
+    const titleContainer = document.createElement("div")
+    titleContainer.className = "title"
+    parent.appendChild(titleContainer)
+
+    const titleText = document.createElement("h1")
+    titleText.textContent = "To-Do List"
+    titleContainer.appendChild(titleText)
+
+    const titleSVG = document.createElement("img")
+    titleSVG.src = "../src/images/main-logo.svg"
+    titleSVG.alt = "Main Logo of a Checkmark"
+    titleSVG.className = "main-logo"
+    titleContainer.appendChild(titleSVG)
+}
+
+buildTitle()
